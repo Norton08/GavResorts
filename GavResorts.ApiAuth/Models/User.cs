c734e2b0edf3e409f674b0748324f5ba11ee2251
Norton Almeida Pontes <nortonalmeida08@gmail.com>
2023-09-23 15:57:19 -0300

    Adicionado Projeto de Autenticação de Api

---
+++ GavResorts.ApiAuth/Models/User.cs
@@ -0,0 +1,9 @@
+namespace GavResorts.ApiAuth.Models;
+
+public class ErrorViewModel
+{
+	public int Id { get; set; }
+	public string Username { get; set; }
+	public string Password { get; set; }
+    public string Role { get; set; }
+}
